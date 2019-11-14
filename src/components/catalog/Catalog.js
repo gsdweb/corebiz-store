263f159f94cd6128b864e32d56042c2795e83b84
gsdweb <dinizcomz@hotmail.com>
2019-11-14 01:08:53 -0300

    Ups Bootstrap

--- src/components/catalog/Catalog.js
+++ src/components/catalog/Catalog.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Logo from "../../assets/logo.png";
 import Product from "./productList";
 
 export default class Catalog extends Component {
@@ -19,33 +18,20 @@ export default class Catalog extends Component {
   render() {
     return (
       <div className="row">
-        <div className="col-sm-4 catalog-top mb-1">
-          <div className="row">
-            <div className="col-lg-12 col-md-8 col-sm-4">
-              <img src={Logo} className="img-fluid catalog-top-logo" />
-            </div>
-            <div className="col-lg-12 col-md-8 col-sm-4">
-              <h2>E-commerce test</h2>
-              <p>
-                The easiest way to shop IBM merchandise items. Simply choose
-                what you want and we'll work with vendors to have your order
-                delivered as soon as possible.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="row catalog-container">
-          <div className="col-sm-4">
+        <div className="row">
+          <div className="col-sm-12 text-center">
             <h3>Merchandise products available for quotation</h3>
           </div>
-          {this.state.products.map(p => (
-            <Product
-              key={p.productId}
-              {...p}
-              addCart={this.props.addCartfunc}
-            />
-          ))}
+          <div className="row">
+            {this.state.products.map(p => (
+              <Product
+                key={p.productId}
+                {...p}
+                addCart={this.props.addCartfunc}
+              />
+            ))}
+          </div>
+
         </div>
       </div>
     );
--- src/components/catalog/productList.js
+++ src/components/catalog/productList.js
@@ -4,14 +4,14 @@ import { Link, Route } from 'react-router-dom';
 
 const Product = ({ productId, productName, imageUrl, price, addCart }) => {
     return (
-        <div className="col-lg-12 col-md-8 catalog" id={productId}>
+        <div className="col-lg-3 col-md-6 col-sm-12 catalog" id={productId}>
             <div className="card">
                 <div className="card-img-top">
                 <Link to={`${productId}`}><img src={imageUrl} alt={productName} width="100%" className="img-fluid" title={productName}/></Link>
                 </div>
                 <div className="card-body">
                     <h4>{productName}</h4>
-                    <p><Link to={`${productId}`}>View</Link></p>
+                    <Link className="btn" to={`${productId}`}>COMPRAR <i class="fas fa-shopping-cart"></i></Link>
                 </div>
             </div>
         </div>
